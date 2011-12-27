dabdd0606aed028aab3d9d15d69148864f7eb04f
GIT loserwn <loserwn@gmail.com>
2011-12-28 01:20:41 +0800

    {Add}

---
+++ protected/config/mail_config.php
@@ -0,0 +1,24 @@
+<?php
+/**
+  * mail dir config
+  */
+define('MAIL_DIR_PATH', '/www/vhost/mail.lanbolee.com/mail_data/') ;
+define('MAIL_INCOMING_DIR', 'incoming') ;
+define('MAIL_WORKING_DIR',  'working')  ;
+define('MAIL_ARCHIVED_DIR', 'archived') ;
+define('MAIL_ATTACHMENTS_DIR', 'attachment') ;
+
+/**
+  * database config 
+  */
+define('MAIL_DB_SERVER_ADDR', 'localhost') ;
+define('MAIL_DB_NAME', 'helpdesk') ;
+define('MAIL_DB_USERNAME', 'root') ;
+define('MAIL_DB_PASSWORD', '123456') ;
+
+/**
+  * mail paser config
+  */
+define('MAIL_MAX_QUERY', 100) ;
+define('MAIL_FILTER_KEYWORD', 'rms-help') ;
+
