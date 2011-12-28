0bdd996e4b40323674f227e741d19fe66d9ddba3
GIT loserwn <loserwn@gmail.com>
2011-12-28 20:30:20 +0800

    [Add]

--- lib/sql/create_tables.sql
+++ lib/sql/create_tables.sql
@@ -1,15 +1,26 @@
 CREATE TABLE `mail_data` (
-	`id` int(11) NOT NULL auto_increment,
-	`mail_id` varchar(255) default NULL,
-	`mail_from` varchar(255) NOT NULL,
-	`mail_to` text NOT NULL,
-	`mail_delivered_to` text NOT NULL,
-	`subject` varchar(255) NOT NULL,
-	`mail_body_text` text NOT NULL,
-	`mail_body_html` text NOT NULL,
-	`mail_attachments` text NOT NULL,
-	`create_at` int(11) NOT NULL,
-	PRIMARY KEY  (`id`),
-	KEY `mail_from` (`mail_from`),
-	KEY `mail_id` (`mail_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 ;
+  `id` int(11) NOT NULL auto_increment,
+  `list_mask` varchar(255) collate utf8_bin default NULL,
+  `time` datetime NOT NULL COMMENT '回复时间',
+  `mail_header` text collate utf8_bin,
+  `mail_from` varchar(255) character set utf8 default NULL,
+  `mail_to` varchar(255) character set utf8 default NULL,
+  `mail_cc` text collate utf8_bin,
+  `title` varchar(255) character set utf8 NOT NULL,
+  `content_text` text collate utf8_bin,
+  `content_html` text collate utf8_bin,
+  `file_name` varchar(255) collate utf8_bin default NULL,
+  `attachment` tinyint(4) default NULL,
+  PRIMARY KEY  (`id`),
+  KEY `list_mask` (`list_mask`)
+) ENGINE=MyISAM AUTO_INCREMENT=258 DEFAULT CHARSET=utf8 COLLATE=utf8_bin ;
+
+CREATE TABLE `mail_attachment` (
+  `id` int(11) NOT NULL auto_increment,
+  `mail_id` int(11) NOT NULL,
+  `path` text collate utf8_bin,
+  `file_name` varchar(255) collate utf8_bin default NULL,
+  `file_type` varchar(255) collate utf8_bin default NULL,
+  `file_description` text collate utf8_bin,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=128 DEFAULT CHARSET=utf8 COLLATE=utf8_bin ;
